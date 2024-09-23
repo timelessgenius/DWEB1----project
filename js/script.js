7d33628ecfc2803ab0a58d7aba2c22cd4397f6c5
Matheus Costa <mcss.pb1@gmail.com>
2024-09-23 07:33:10 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,21 +1,17 @@
-// Inicializar o carrinho com os dados do localStorage (se houver)
 let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
 let total = carrinho.reduce((sum, produto) => sum + produto.preco, 0);
 
-// Função para adicionar produtos ao carrinho
 function adicionarAoCarrinho(nome, preco) {
     const produto = { nome, preco };
     carrinho.push(produto);
     total += preco;
     
-    // Salvar carrinho no localStorage
     localStorage.setItem('carrinho', JSON.stringify(carrinho));
 
     alert(`${nome} adicionado ao carrinho!`); // Mensagem de confirmação
     atualizarCarrinho();
 }
 
-// Função para atualizar a visualização do carrinho
 function atualizarCarrinho() {
     const carrinhoLista = document.getElementById('carrinho');
     const totalSpan = document.getElementById('total');
@@ -35,7 +31,6 @@ function atualizarCarrinho() {
     }
 }
 
-// Função para redirecionar para a página do carrinho
 function irParaCarrinho() {
     window.location.href = 'html/carrinho.html';
 }
